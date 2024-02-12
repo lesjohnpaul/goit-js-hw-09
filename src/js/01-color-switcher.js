c44cc3d7e37af75e608aed39e2d0e2162b830bb0
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-02-13 01:24:07 +0800

    Color switcher JS

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId;
 
+function changeBackgroundColor() {
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+}
+
+startButton.addEventListener('click', function () {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  startButton.disabled = true;
+  startButton.classList.add('disabled');
+  stopButton.disabled = false;
+  stopButton.classList.remove('disabled');
+});
+
+stopButton.addEventListener('click', function () {
+  clearInterval(intervalId);
+  stopButton.disabled = true;
+  stopButton.classList.add('disabled');
+  startButton.disabled = false;
+  startButton.classList.remove('disabled');
+});
